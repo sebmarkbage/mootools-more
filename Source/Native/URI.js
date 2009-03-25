8def5588c18e310661346fda70816a33ca746b7f
Sebastian Markbage <sebastian@calyptus.eu>
2009-03-25 02:34:59 +0100

    Clean up and rename of extensible URI, added specs

--- Source/Native/URI.js
+++ Source/Native/URI.js
@@ -1,18 +1,248 @@
-/*
+﻿/*
 Script: URI.js
-	Provides methods useful in managing the window location and uris.
+	Class to parse and modify URIs.
 
 	License:
 		MIT-style license.
 
-	String subclass based on:
-		http://webreflection.blogspot.com/2008/10/subclassing-javascript-native-string.html
-
 	Authors:
-		Aaron Newton, Lennart Pilon, Valerio Proietti
+		Sebastian Markbåge
 */
 
+var URI = new Class({
+
+	initialize: function(baseURI, relativeURI){
+		if(!relativeURI && relativeURI !== 0){
+			if(!URI.Base){
+				URI.Base = new URI({}, window.location.href);
+				var bases = document.getElementsByTagName('base');
+				for(var i=bases.length-1;i>=0;i--) if(bases[i].href){ URI.Base = new URI(bases[i].href); break; }
+			}
+			relativeURI = baseURI;
+			baseURI = URI.Base;
+			if(!relativeURI && relativeURI !== 0) return baseURI;
+		}
+		
+		if(relativeURI.scheme) return $extend(this, relativeURI);
+
+		relativeURI = relativeURI.toString();
+		
+		var match;
+		if(match = /^([a-zA-Z]+)\:/.exec(relativeURI)){
+			this.scheme = URI.Schemes[match[1].toLowerCase()] || URI.Schemes.unknown;
+		} else {
+			$extend(this, new URI(baseURI));
+			var relative = this.scheme.relative;
+			if(relative && relative.set){
+				relative.set.apply(this, [relativeURI]);
+				return this;
+			}
+		}
+		return this.set('href', relativeURI);
+	},
+	
+	get: function(part){
+		var prop = this.scheme ? this.scheme[part] : false;
+		return prop && prop.get ? prop.get.apply(this, $A(arguments).slice(1)) : this[part];
+	},
+	
+	set: function(part, value){
+		var args = $A(arguments).slice(1), prop = this.scheme ? this.scheme[part] : false;
+		if(prop) prop.set.apply(this, args); else this[part] = value;
+		return this;
+	},
+	
+	getData: function(key){ var obj = this.get('data') || {}; return key ? obj[key] : obj; },
+	setData: function(values, merge){
+		if(typeof values == 'string' && typeof merge != undefined){
+			var key = values;
+			values = this.getData();
+			values[key] = merge;
+			merge = false;
+		}
+		return this.set('data', (merge ? $merge(this.getData(), values) : values) || {});
+	},
+	
+	toAbsolute: function(baseURI){ return this.get('absolute', baseURI) || this.get('href'); },
+	toRelative: function(baseURI){ return this.get('relative', baseURI) || this.get('href'); },
+
+	toString: function(){ return this.get('href'); }
+});
+
+(function(){
+
+// Schemes
+
+var matchThis = function(r, m){
+	return function(v){
+		var self = this, match = r.exec(v);
+		if(match) m.each(function(k,i){ self[k] = match[i+1]; });
+	};
+};
+
+var createDataProperty = function(key){
+	return {
+		get: function(){
+			var d = this.get(key);
+			return d && d.length > 1 ? decodeURI(d.substr(1)).parseQueryString(false, false) : false; 
+		},
+		set: function(obj){
+			var nq = '';
+			for (var k in obj){
+				if(nq != '') nq += '&';
+				nq += encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]);
+			}
+			return this.set(key, nq != '' ? '?' + nq : nq);
+		}
+	};			
+};
+
+var setRelative = function(value){
+	var uri = this, oldDir = uri.directory;
+
+	matchThis(/^(\.\.?$|(?:[^?#\/]*\/)*)([^?#]*)(\?[^#]*)?(#.*)?/, ['directory', 'file', 'search', 'hash']).apply(uri, [value]);
+	
+	var dir = uri.directory;
+	if(dir){
+		var baseDir = !oldDir || /^\/.?/.test(dir) ? [] : oldDir.replace(/\/$/, '').split('/'),
+			relDir = dir.replace(/\/$/, '').split('/');
+		relDir.each(function(d, i){
+			if(d == '..'){
+				if(baseDir.length > 1 || (baseDir.length > 0 && baseDir[0] != '')) baseDir.pop();
+			} else if(d != '.')
+				baseDir.push(d);
+		});
+		uri.directory = baseDir.join('/') + '/';
+	}
+	else
+		uri.directory = oldDir || '/';
+};
+
+URI.Schemes = {
+	http: {
+		href: {
+			get: function(){
+				var uri = this, defaultPort = URI.DefaultPorts[uri.protocol.substr(0, uri.protocol.length - 1).toLowerCase()];
+				return uri.protocol + '//' + (uri.user ? uri.user + (uri.password ? ':' + uri.password : '') + '@' : '') +
+					   (uri.hostname || '') + (uri.port && uri.port != defaultPort ? ':' + uri.port : '') +
+					   (uri.directory || '/') + (uri.file || '') + (uri.search || '') + (uri.hash || '');
+			},
+			set: function(value){
+				var uri = this;
+				matchThis(
+					/^([a-zA-Z]+:)(?:\/\/(?:(?:([^:@]*):?([^:@]*))?@)?([^:\/?#]+)?(?::(\d*))?)?((?:[^?#\/]*\/)*)([^?#]*)(\?[^#]*)?(#.*)?/,
+					['protocol', 'user', 'password', 'hostname', 'port', 'directory', 'file', 'search', 'hash']
+				).apply(uri, [value]);
+				uri.port = uri.port || URI.DefaultPorts[uri.protocol.substr(0, uri.protocol.length - 1).toLowerCase()];
+				uri.directory = uri.directory || '/';
+			}
+		},
+		
+		pathname: {
+			get: function(uri){ return uri.directory + (uri.file || ''); },
+			set: matchThis(/^([^\/]*)(.*)$/, ['directory', 'file'])
+		},
+		
+		relative: {
+			get: function(baseURI){
+				var uri = this;
+				baseURI = baseURI ? new URI(uri, baseURI) : new URI();
+				if(!uri.directory || !baseURI.directory || uri.protocol != baseURI.protocol || uri.hostname != baseURI.hostname || uri.port != baseURI.port)
+					return uri.get('href');
+
+				var baseDir, relDir, path = '', o;
+				baseDir = baseURI.directory.split('/');
+				relDir = uri.directory.split('/');
+				
+				for(o = 0; o < baseDir.length && o < relDir.length && baseDir[o] == relDir[o]; o++);
+				for(var i = 0; i < baseDir.length - o - 1; i++) path += '../';
+				for(var i = o; i < relDir.length - 1; i++) path += relDir[i] + '/';
+					
+				return (path || (uri.file ? '' : './')) + (uri.file || '') + (uri.search || '') + (uri.hash || '');
+			},
+			set: setRelative
+		},
+		
+		absolute: {
+			get: function(baseURI){
+				var uri = this;
+				baseURI = baseURI ? new URI(uri, baseURI) : new URI();
+				if(!uri.directory || !baseURI.directory || uri.protocol != baseURI.protocol || uri.hostname != baseURI.hostname || uri.port != baseURI.port)
+					return uri.get('href');
+				return uri.directory + (uri.file || '') + (uri.search || '') + (uri.hash || '')
+			},
+			set: setRelative
+		},
+		
+		data: createDataProperty('search')
+	},
+	mailto: {
+		href: {
+			get: function(){ return this.protocol + this.username + '@' + this.hostname + (this.headers || ''); },
+			set: matchThis(/^([a-z]+:)([^\.:@]+(?:\.[^:@]+)*)@((?:[^?:\.]+\.)*[^?:\.]+)(\?.*)?/i, ['protocol', 'username', 'hostname', 'headers'])
+		},
+		
+		email: {
+			get: function(){ return this.username + '@' + this.hostname; },
+			set: matchThis(/^([^\.:@]+(?:\.[^:@]+)*)@((?:[^?:\.]+\.)*[^?:\.]+)$/, ['username', 'hostname'])
+		},
+		
+		subject: {
+			get: function(){ return this.getData('subject'); },
+			set: function(value){ this.setData('subject', value); }
+		},
+		
+		body: {
+			get: function(){ return this.getData('body'); },
+			set: function(value){ this.setData('body', value); }
+		},
+		
+		data: createDataProperty('headers')
+	},
+	javascript: {
+		href: {
+			get: function(uri){ return uri.protocol + (uri.script || '').toString().replace(/\r?\n/g, ' '); },
+			set: matchThis(/^([a-z]+:)(.*)$/, ['protocol', 'script'])
+		}
+	},
+	unknown: {}
+};
+
+// More URL schemes
+
+['https','ftp','file','rtsp','mms'].each(function(scheme){
+	URI.Schemes[scheme] = URI.Schemes.http;
+});
+
+// Method aliases
+
+var aliases = {
+	getPath: function(){ return this.get('pathname'); },
+	setPath: function(value){ return this.set('pathname', value); },
+	getUsername: function(){ return this.get('user') || this.get('username'); },
+	setUsername: function(value){ return this.set('user', value).set('username', value); },
+	getQuery: function(){ return this.get('search') || this.get('headers'); },
+	setQuery: function(value){ return this.set('search', value).set('headers', value); }
+};
+['protocol', 'password', 'hostname', 'port', 'directory', 'file', 'hash', 'subject', 'body', 'script', 'email'].each(function(part){
+	var capitalizedPart = part.capitalize();
+	aliases['get' + capitalizedPart] = function(){ return this.get.apply(this, [part].extend(arguments)); }
+	aliases['set' + capitalizedPart] = function(){ return this.set.apply(this, [part].extend(arguments)); }
+});
+
+URI.implement(aliases);
+
+// Default ports
+
+URI.DefaultPorts = { http: 80, https: 443, ftp: 21 };
+
+})();
+
 String.implement({
+
+	toURI: function(baseURI){
+		return new URI(baseURI, this);
+	},
 	
 	parseQueryString: function(encodeKeys, encodeValues){
 		encodeKeys = $pick(encodeKeys, true);
@@ -32,109 +262,5 @@ String.implement({
 			return $chk(set.split('=')[1]);
 		}).join('&');
 	}
-	
-});
-
-var URI = new Native({
-
-	initialize: function(uri){
-		this.value = uri || document.location.href || '';
-		this.assertLength();
-	}
 
 });
-
-URI.prototype = new String;
-
-(function(){
-
-	var reg = /^(?:(\w+):\/\/)?(?:([^\/:?#]*))?(?::(\d+))?([^#?]*)(?:\?([^#]*))?(?:#(.*))?$/;
-	var parts = ['full', 'protocol', 'host', 'port', 'path', 'query', 'fragment'];
-
-	URI.implement({
-
-		assertLength: function(){
-			if (this.length != this.value.length) this.length = this.value.length;
-		},
-
-		toString: function(){
-			return this.value;
-		},
-
-		valueOf: function(){
-			return this.value;
-		},
-
-		parseURI: function(){ 
-			var bits = this.value.match(reg).associate(parts);
-			delete bits.trash;
-			return bits;		
-		},
-
-		set: function(part, value){
-			if (part == 'data') return this.setData(value);
-			var bits = this.parseURI();
-			bits[part] = value;
-			this.combine(bits);
-			return this;
-		},
-
-		get: function(part){
-			return (part == 'data') ? this.getData() : this.parseURI()[part];
-		},
-
-		combine: function(bits){
-			bits = bits || this.parseURI();
-			var result = '';
-			if (bits.protocol) result += bits.protocol + '://';
-			if (bits.host) result += bits.host;
-			if ($defined(bits.port)) result += ':' + bits.port;
-			if (bits.path) result += bits.path;
-			if (bits.query) result += '?' + bits.query;
-			if (bits.fragment) result += '#' + bits.fragment;
-			this.value = result;
-			this.assertLength();
-			return this;
-		},
-
-		getData: function(key){
-			var qs = this.get('query');
-			if (!$chk(qs)) return key ? null : {};
-			var obj = decodeURI(qs).parseQueryString(false, false); 
-			return key ? obj[key] : obj;
-		},
-
-		setData: function(values, merge){
-			var merged = merge ? $merge(this.getData(), values) : values;
-			var newQuery = '';
-			for (key in merged) newQuery += encodeURIComponent(key) + '=' + encodeURIComponent(merged[key]) + '&';
-			return this.set('query', newQuery.substring(0, newQuery.length-1));
-		},
-
-		clearData: function(){
-			this.set('query', '');
-		},
-
-		go: function(){
-			document.location.href = this.value;
-		}
-
-	});
-
-	var methods = {};
-
-	parts.each(function(part){
-
-		methods['get' + part.capitalize()] = function(){
-			return this.get(part);
-		};
-
-		methods['set' + part.capitalize()] = function(value){
-			return this.set(part, value);
-		};
-
-	});
-
-	URI.implement(methods);
-
-})();
--- Specs/Native/URI.js
+++ Specs/Native/URI.js
@@ -33,53 +33,369 @@ License:
 
 	});
 
-	var url = 'http://www.test.com:8383/this/is/the/path.html?query=value#anchor';
+	var uri;
 
-	describe('URL.get(\'host\')', {
 
-			'should get the domain of a url': function(){
-				value_of(new URI(url.toString()).get('host')).should_be('www.test.com');
-			}
+	describe('String.toURI using relative path', {
+
+		before: function(){
+			uri = '/mydirectory/myfile.html?myquery=true#myhash'.toURI('http://myuser:mypass@www.calyptus.eu:8080');
+		},
+
+		'URI.toString() should be same as input combined': function(){
+			value_of(uri.toString()).should_be('http://myuser:mypass@www.calyptus.eu:8080/mydirectory/myfile.html?myquery=true#myhash');
+		},
+		
+		'should have a protocol ending with colon': function(){
+			value_of(uri.getProtocol()).should_be('http:');		
+		},
+		
+		'should have a user set': function(){
+			value_of(uri.getUsername()).should_be('myuser');
+		},
+		
+		'should have a password set': function(){
+			value_of(uri.getPassword()).should_be('mypass');
+		},
+		
+		'should have a full hostname': function(){
+			value_of(uri.getHostname()).should_be('www.calyptus.eu');
+		},
+		
+		'should have an explicit port': function(){
+			value_of(uri.getPort()).should_be(8080);
+		},
+		
+		'should have a directory': function(){
+			value_of(uri.getDirectory()).should_be('/mydirectory/');
+		},
+		
+		'should have a file name': function(){
+			value_of(uri.getFile()).should_be('myfile.html');
+		},
+		
+		'should have a search set': function(){
+			value_of(uri.getQuery()).should_be('?myquery=true');
+		},
+		
+		'should have a hash set': function(){
+			value_of(uri.getHash()).should_be('#myhash');
+		}
+	});
+	
+	describe('URI initialize', {
+
+		'new URI() should return the current location': function(){
+			value_of(new URI()).should_be(window.location.href);
+		},
+
+		'new URI(\'../otherfolder\').toRelative() should return a folder up from the current location': function(){
+			value_of(new URI('../otherfolder').toRelative()).should_be('../otherfolder');
+		},
+
+		'new URI(\'../otherfolder\').toRelative(currentLocation) should return a folder up from the current location': function(){
+			value_of(new URI('../otherfolder').toRelative(window.location)).should_be('../otherfolder');
+		},
+
+		'new URI(\'http://www.calyptus.eu\') should return itself with a trailing slash': function(){
+			value_of(new URI('http://www.calyptus.eu')).should_be('http://www.calyptus.eu/');
+		},
+
+		'new URI(\'http://www.calyptus.eu/\') should return itself': function(){
+			value_of(new URI('http://www.calyptus.eu/')).should_be('http://www.calyptus.eu/');
+		},
+		
+		'new URI(\'http://www.calyptus.eu/\', \'./mydirectory/myfile.html\') should return http://www.calyptus.eu/mydirectory/myfile.html': function(){
+			value_of(new URI('http://www.calyptus.eu/', './mydirectory/myfile.html')).should_be('http://www.calyptus.eu/mydirectory/myfile.html');
+		},
+
+		'new URI(\'http://www.calyptus.eu\', \'mydirectory/myfile.html\') should return http://www.calyptus.eu/mydirectory/myfile.html': function(){
+			value_of(new URI('http://www.calyptus.eu', 'mydirectory/myfile.html')).should_be('http://www.calyptus.eu/mydirectory/myfile.html');
+		},
+		
+		'new URI(\'http://www.calyptus.eu/mydirectory/#\', \'../myfile.html\') should return http://www.calyptus.eu/myfile.html': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory/#', '../myfile.html')).should_be('http://www.calyptus.eu/myfile.html');
+		},
+		
+		'new URI(\'http://www.calyptus.eu/mydirectory/mydirectory2/\', \'../../myfile.html\') should return http://www.calyptus.eu/myfile.html': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory/mydirectory2/', '../../myfile.html')).should_be('http://www.calyptus.eu/myfile.html');
+		},
+
+		'new URI(\'http://www.calyptus.eu/mydirectory/mydirectory2/\', \'../test/../myfile.html\') should return http://www.calyptus.eu/mydirectory/myfile.html': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory/mydirectory2/', '../test/../myfile.html')).should_be('http://www.calyptus.eu/mydirectory/myfile.html');
+		},
+		
+		'new URI(\'http://www.calyptus.eu/\', \'http://otherdomain/mydirectory/myfile.html\') should return http://otherdomain/mydirectory/myfile.html': function(){
+			value_of(new URI('http://www.calyptus.eu/', 'http://otherdomain/mydirectory/myfile.html')).should_be('http://otherdomain/mydirectory/myfile.html');
+		},
+		
+		'new URI(\'http://www.calyptus.eu/mydirectory2/myfile.html\', \'/mydirectory/myfile.html\') should return http://www.calyptus.eu/mydirectory/myfile.html': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory2/myfile.html', '/mydirectory/myfile.html')).should_be('http://www.calyptus.eu/mydirectory/myfile.html');
+		},
+
+		'new URI(\'http://www.calyptus.eu/mydirectory2/\', \'mydirectory/myfile.html\') should return http://www.calyptus.eu/mydirectory2/mydirectory/myfile.html': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory2/myfile.html', 'mydirectory/myfile.html')).should_be('http://www.calyptus.eu/mydirectory2/mydirectory/myfile.html');
+		},
+
+		'new URI(\'http://www.calyptus.eu/mydirectory2/\', \'mydirectory\') should return http://www.calyptus.eu/mydirectory2/mydirectory': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory2/myfile.html', 'mydirectory')).should_be('http://www.calyptus.eu/mydirectory2/mydirectory');
+		},
+
+		'new URI(\'http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html\', \'..\') should return http://www.calyptus.eu/mydirectory/': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html', '..')).should_be('http://www.calyptus.eu/mydirectory/');
+		}
 
 	});
+	
+	describe('URI http protocol', {
 
-	describe('URL.getData', {
+		before: function(){
+			uri = new URI('http://myuser:mypass@www.calyptus.eu:80/mydirectory/myfile.html?myquery=true#myhash');
+		},
+		
+		'toString() should be same as input but cleaned up (port removed)': function(){
+			value_of(uri.toString()).should_be('http://myuser:mypass@www.calyptus.eu/mydirectory/myfile.html?myquery=true#myhash');
+		},
 
-			'should get the query string values from a url': function(){
-				value_of(new URI(url.toString()).getData()['query']).should_be('value');
-			},
+		'should have a protocol ending with colon': function(){
+			value_of(uri.getProtocol()).should_be('http:');		
+		},
+		
+		'should have a user set': function(){
+			value_of(uri.getUsername()).should_be('myuser');
+		},
+		
+		'should have a password set': function(){
+			value_of(uri.getPassword()).should_be('mypass');
+		},
+		
+		'should have a full hostname': function(){
+			value_of(uri.getHostname()).should_be('www.calyptus.eu');
+		},
+		
+		'should have the default port': function(){
+			value_of(uri.getPort()).should_be(80);
+		},
+		
+		'should have a directory': function(){
+			value_of(uri.getDirectory()).should_be('/mydirectory/');
+		},
+		
+		'should have a file name': function(){
+			value_of(uri.getFile()).should_be('myfile.html');
+		},
+		
+		'should have a search set': function(){
+			value_of(uri.getQuery()).should_be('?myquery=true');
+		},
+		
+		'should have a hash set': function(){
+			value_of(uri.getHash()).should_be('#myhash');
+		}
+	});
+	
+	describe('URI file protocol', {
 
-			'should get the query string value from a url': function(){
-				value_of(new URI(url.toString()).getData('query')).should_be('value');
-			}
-
+		before: function(){
+			uri = new URI('file:///mytopdirectory/mydirectory/myfile.html?myquery=true#myhash');
+		},
+		
+		'protocol should be file with a trailing colon': function(){
+			value_of(uri.getProtocol()).should_be('file:');
+		},
+		
+		'user should be undefined': function(){
+			value_of(uri.getUsername()).should_be(undefined);
+		},
+		
+		'password should be undefined': function(){
+			value_of(uri.getPassword()).should_be(undefined);
+		},
+		
+		'hostname should be undefined': function(){
+			value_of(uri.getHostname()).should_be(undefined);
+		},
+		
+		'port should be undefined': function(){
+			value_of(uri.getPort()).should_be(undefined);
+		},
+		
+		'directory should be should be set with trailing slash': function(){
+			value_of(uri.getDirectory()).should_be('/mytopdirectory/mydirectory/');
+		},
+		
+		'file name should be myfile.html': function(){
+			value_of(uri.getFile()).should_be('myfile.html');
+		},
+		
+		'search should be set': function(){
+			value_of(uri.getQuery()).should_be('?myquery=true');
+		},
+		
+		'hash should be set': function(){
+			value_of(uri.getHash()).should_be('#myhash');
+		}
 	});
 
-	describe('URI.get(\'domain\')', {
+	describe('URI mailto protocol', {
 
-			'should get the protocol from a url': function(){
-				value_of(new URI(url.toString()).get('protocol')).should_be('http');
-			}
+		before: function(){
+			uri = new URI('mailto:info@calyptus.eu?subject=This%20rocks');
+		},
 
+		'toString() should be same as input': function(){
+			value_of(uri.toString()).should_be('mailto:info@calyptus.eu?subject=This%20rocks');
+		},
+		
+		'protocol should be mailto with trailing colon': function(){
+			value_of(uri.getProtocol()).should_be('mailto:');
+		},
+
+		'email should be the full email without protocol and headers': function(){
+			value_of(uri.getEmail()).should_be('info@calyptus.eu');
+		},
+
+		'username should be the part before @ but after mailto:': function(){
+			value_of(uri.getUsername()).should_be('info');
+		},
+
+		'hostname should be the domain name': function(){
+			value_of(uri.getHostname()).should_be('calyptus.eu');
+		},
+
+		'headers should be should be everything after and including the question mark': function(){
+			value_of(uri.getQuery()).should_be('?subject=This%20rocks');
+		},
+
+		'subject should be the "subject" part of the headers': function(){
+			value_of(uri.getSubject()).should_be('This rocks');
+		},
+		
+		'subject should be the "body" part of the headers, in this case undefined': function(){
+			value_of(uri.getBody()).should_be(undefined);
+		},
+
+		'All http/https/ftp/file specific properties should be undefined': function(){
+			value_of(uri.get('user')).should_be(undefined);
+			value_of(uri.getPassword()).should_be(undefined);
+			value_of(uri.getPort()).should_be(undefined);
+			value_of(uri.getDirectory()).should_be(undefined);
+			value_of(uri.getFile()).should_be(undefined);
+			value_of(uri.get('search')).should_be(undefined);
+			value_of(uri.getHash()).should_be(undefined);
+		}
 	});
 
-	describe('URI.get(\'port\')', {
+	describe('URI methods', {
 
-			'should get the port from a url': function(){
-				value_of(new URI(url.toString()).get('port')).should_be('8383');
-			}
+		before_all: function(){
+			uri = new URI('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html', false);
+		},
+		
+		'URI.toString() should be same as input': function(){
+			value_of(uri.toString()).should_be('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html');
+		},
+		
+		'URI.toRelative(string)': function(){
+			value_of(uri.toRelative('http://www.calyptus.eu/mydirectory/myfile.html')).should_be('mydirectory2/myfile.html');
+		},
 
+		'URI.toRelative(string)': function(){
+			value_of(uri.toRelative('http://www.calyptus.eu/mydirectory/')).should_be('mydirectory2/myfile.html');
+		},
+
+		'URI.toRelative(uri)': function(){
+			value_of(uri.toRelative(new URI('http://www.calyptus.eu/', 'mydirectory/myfile.html'))).should_be('mydirectory2/myfile.html');
+		},
+
+		'URI.toAbsolute(string)': function(){
+			value_of(uri.toAbsolute('http://www.calyptus.eu/mydirectory/myfile.html')).should_be('/mydirectory/mydirectory2/myfile.html');
+		},
+
+		'URI.toAbsolute(uri)': function(){
+			value_of(uri.toAbsolute(new URI('http://www.calyptus.eu/', 'mydirectory/myfile.html'))).should_be('/mydirectory/mydirectory2/myfile.html');
+		},
+
+		'URI.toRelative(string) on parent': function(){
+			value_of(uri.toRelative('http://www.calyptus.eu/test/myfile.html')).should_be('../mydirectory/mydirectory2/myfile.html');
+		},
+
+		'URI.toRelative(string) on different host': function(){
+			value_of(uri.toRelative('http://otherdomain/mydirectory/myfile.html')).should_be('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html');
+		},
+
+		'URI.toAbsolute(string) on different host': function(){
+			value_of(uri.toAbsolute('http://otherdomain/mydirectory/myfile.html')).should_be('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html');
+		},
+
+		'URI.toRelative(string) on different port': function(){
+			value_of(uri.toRelative('http://www.calyptus.eu:81/mydirectory/myfile.html')).should_be('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html');
+		},
+
+		'URI.toAbsolute(string) on different port': function(){
+			value_of(uri.toAbsolute('http://www.calyptus.eu:81/mydirectory/myfile.html')).should_be('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html');
+		},
+		
+		'URI.toRelative(string) with query': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html?myquery=q', false).toRelative('http://www.calyptus.eu/mydirectory/myfile.html')).should_be('mydirectory2/myfile.html?myquery=q');
+		},
+
+		'URI.toAbsolute(string) with query': function(){
+			value_of(new URI('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html?myquery=q', false).toAbsolute('http://www.calyptus.eu/mydirectory/myfile.html')).should_be('/mydirectory/mydirectory2/myfile.html?myquery=q');
+		},
+
+		'URI.toRelative(string) to same file': function(){
+			value_of(uri.toRelative('http://www.calyptus.eu/mydirectory/mydirectory2/myfile.html')).should_be('myfile.html');
+		},
+
+		'URI.toRelative(string) to same path': function(){
+			value_of(new URI('http://www.calyptus.eu').toRelative('http://www.calyptus.eu')).should_be('./');
+		},
+		
+		'new URI(\'../otherfolder\').toRelative() should return the same as input': function(){
+			value_of(new URI('../otherfolder').toRelative(window.location)).should_be('../otherfolder');
+		},
+
+		'new URI(\'../otherfolder\').toRelative(window.location) should return the same as input': function(){
+			value_of(new URI('../otherfolder').toRelative(window.location)).should_be('../otherfolder');
+		},
+		
+		'URI.setData({ keyName: \'my value\' }) should return ?keyName=my%20value as the query': function(){
+			uri.setData({ keyName: 'my value' });
+			value_of(uri.get('search')).should_be('?keyName=my%20value');
+		},
+		
+		'URI.getData() should return an object with the value set above': function(){
+			value_of(uri.getData().keyName).should_be('my value');
+		},
+
+		'URI.getData(\'keyName\') should return the string with the value set above': function(){
+			value_of(uri.getData('keyName')).should_be('my value');
+		}
+		
 	});
 
-	describe('URI.setData', {
+	describe('URI where string is expected', {
 
-			'should set query string values': function(){
-				value_of(new URI('www.test.com').setData({a: 'b'}).toString()).should_be('www.test.com?a=b');
-			},
-
-			'should merge url query string values': function(){
-				value_of(new URI('www.test.com?x=y&a=b').setData({a: 'c'}, true).toString()).should_be('www.test.com?x=y&a=c');
-			}
+		'Request self should work with an URI object': function(){
+			new Request({ url: new URI() }).get();
+		},
+		
+		'A HREF should take an URI object': function(){
+			value_of(new Element('a').set('href', new URI()).get('href')).should_be(new URI().toString());
+		},
+		
+		'post-concatenation with string': function(){
+			value_of(new URI('http://www.calyptus.eu/') + '?test').should_be('http://www.calyptus.eu/?test');
+		},
+		
+		'pre-concatenation with string': function(){
+			value_of('URL: ' + new URI('http://www.calyptus.eu/')).should_be('URL: http://www.calyptus.eu/');
+		},
+		
+		'regexp test': function(){
+			value_of(/^http/.test(new URI('http://www.calyptus.eu/'))).should_be(true);
+		}
 
 	});
 
